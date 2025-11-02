24a4a48d98f382dbc11e4edfec54d3fc5747eb9b
yangzehao <yangzehao2001@outlook.com>
2025-11-02 16:36:58 +0800

    提交作业

--- Assignment-3-CGCFG/CFGA.h
+++ Assignment-3-CGCFG/CFGA.h
@@ -2,10 +2,14 @@
  * ICFG.h
  * @author kisslune 
  */
-
 #ifndef ANSWERS_ICFG_H
 #define ANSWERS_ICFG_H
 
+// 新增：LLVM类型适配（必须在包含SVF头文件之前！）
+#include <llvm/Transforms/Utils/UnifyFunctionExitNodes.h>  // 引入新版类型
+typedef llvm::UnifyFunctionExitNodesPass llvm::UnifyFunctionExitNodesLegacyPass;  // 映射旧名到新名
+
+// 原有SVF头文件（现在会用上面的映射）
 #include "Graphs/SVFG.h"
 #include "SVF-LLVM/SVFIRBuilder.h"
 
@@ -15,10 +19,8 @@ public:
     explicit CFGAnalysis(SVF::ICFG *icfg);
     void analyze(SVF::ICFG *icfg);
     void dumpPaths();
-
 protected:
     void recordPath(const std::vector<unsigned> &path);
-
     std::stack<unsigned> callStack;
     std::set<unsigned> sources;
     std::set<unsigned> sinks;
